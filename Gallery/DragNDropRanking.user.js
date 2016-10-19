7b11a8f4898169f80f2cd2934ad5267062789d7c
kgarnett <kgarnett@sfu.ca>
2016-10-18 21:30:01 -0700

    Updated Version

--- Gallery/DragNDropRanking.user.js
+++ Gallery/DragNDropRanking.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoHelper - Gallery Organizer
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       bajuwa
 // @match        http://www.neopets.com/gallery/*
